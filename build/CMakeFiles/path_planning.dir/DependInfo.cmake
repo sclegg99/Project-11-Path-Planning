174b647be050520de56e24742e2e3a88ba2c6222
Scott Clegg <scott.c@cox.net>
2018-02-06 14:11:32 -0700

    Update CMaketest

--- build/CMakeFiles/path_planning.dir/DependInfo.cmake
+++ build/CMakeFiles/path_planning.dir/DependInfo.cmake
@@ -12,7 +12,6 @@ set(CMAKE_CXX_COMPILER_ID "Clang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../include"
   "/usr/local/include"
   "/usr/local/opt/openssl/include"
   )
